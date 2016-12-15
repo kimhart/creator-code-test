5e8c6e55792b3c909c8107290df0ca3f02bea176
Kim Hart <kimhart90@gmail.com>
2016-12-15 18:39:50 -0500

    form slider

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-// Vimeo Player Presets
+// // Vimeo Player Presets
 
 var iframe = document.querySelector('iframe');
 var player = new Vimeo.Player(iframe);
 
 player.on('play', function() {
-    console.log('played the video!');
+    console.log('playing!');
 });
 
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
+
+
+// Cue Input Form
+
+var addCueButton = document.getElementById('add-cue');
+var cueForm = document.getElementById('cue-form');
+var message = document.getElementById('message').value;
+
+addCueButton.addEventListener('click', function(){
+  player.pause();
+  player.getCurrentTime().then(function(seconds) {
+    console.log(Math.floor(seconds));
+  }).catch(function(error) {
+    console.log(error);
+  });
+
+  cueForm.classList.add('cue-form--open', 'fadeInDown');
+})
+
+cueForm.onsubmit = function(e){
+  e.preventDefault();
+  console.log('added cue')
+}
+
 
 
 
